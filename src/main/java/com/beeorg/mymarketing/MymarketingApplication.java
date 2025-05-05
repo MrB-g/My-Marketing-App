835db48b866fdd0ebbf81179da2fc2aabc43a5c9
MrB-g <thura.bb.2003@gmail.com>
2025-05-05 11:30:45 +0630

    [DEV] Initiate spring boot projec and add db configuration.

---
+++ src/main/java/com/beeorg/mymarketing/MymarketingApplication.java
@@ -0,0 +1,12 @@
+package com.beeorg.mymarketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymarketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymarketingApplication.class, args);
+	}
+}
